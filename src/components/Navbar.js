0729ecccd100f71b315cbea8009939f7c4139cf3
Jérémie Laval <jeremie.laval@gmail.com>
2019-06-30 21:40:10 -0400

    Implement burger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,29 @@ import { Link } from 'react-scroll'
 
 class Navbar extends Component {
 	render() {
+		function handleBurgerClick (e) {
+			const ClassSwitch = "expanded";
+
+			e.preventDefault ();
+			let links = document.getElementById ("nav-links");
+			if (links.classList.contains (ClassSwitch))
+				links.classList.remove (ClassSwitch);
+			else
+				links.classList.add (ClassSwitch);
+			let burger = document.getElementById ("nav-burger");
+			if (burger.classList.contains (ClassSwitch)) {
+				burger.classList.remove (ClassSwitch);
+				burger.classList.replace ("fa-times", "fa-hamburger");
+			} else {
+				burger.classList.add (ClassSwitch);
+				burger.classList.replace ("fa-hamburger", "fa-times");
+			}
+		}
+
 		return (
 			<div className="nav">
-				<i className="burger-icon fas fa-hamburger"></i>
-				<ul>
+				<a id="nav-burger" className="burger-icon fas fa-hamburger" onClick={handleBurgerClick}></a>
+				<ul id="nav-links">
 					<li><Link to="home" className="menu-item">Home</Link></li>
 					<li><Link to="about" className="menu-item">About</Link></li>
 					<li><Link to="resume" className="menu-item">Resume</Link></li>
