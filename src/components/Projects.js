cf1dbb7d94f027260b96dbfec54a1f83cf15a9e6
Kerry Jackson <kerry.jackson108@gmail.com>
2019-07-04 15:40:58 -0400

    Tweak project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,13 +15,10 @@ class Projects extends Component {
 		videos.forEach((video, index) => {
 			overlay[index].addEventListener("mouseenter", () => {
 				video.play();
-				text.visibility = "block";
-
 			})
 
 			overlay[index].addEventListener("mouseleave", () => {
 				video.pause();
-
 			})
 
 			overlay[index].addEventListener("touchstart", () => {
@@ -45,9 +42,9 @@ class Projects extends Component {
 							<div class="card">
 								<video src="clock.mp4" width="507.66" loop={true} alt="video of clock project" />
 								<div class="card-img-overlay">
-									<a class="btn btn-md" href="https://www.kerry.dev/clock" target="blank">View Live</a>
-									<a class="btn btn-md" href="https://github.com/kerryja/JS30/tree/master/2%20-%20CSS%20Clock" target="blank">GitHub</a>
-									<p id="text" style={{ display: "none" }}>some text here</p>
+									<a class="btn btn-sm" href="https://www.kerry.dev/clock" target="blank">View Live</a>
+									<a class="btn btn-sm" href="https://github.com/kerryja/JS30/tree/master/2%20-%20CSS%20Clock" target="blank">GitHub</a>
+									<div class="text">Clock built entirely with CSS and JS</div>
 								</div>
 							</div>
 						</div>
@@ -55,9 +52,9 @@ class Projects extends Component {
 							<div class="card">
 								<video src="draw.mp4" width="507.66" loop={true} alt="video of drawing project" />
 								<div class="card-img-overlay">
-									<a class="btn btn-md" href="https://kerry.dev/draw/" target="blank">View Live</a>
-									<a class="btn btn-md" href="https://github.com/kerryja/JS30/tree/master/7%20-%20HTML%20Canvas" target="blank">GitHub</a>
-									<p id="text" style={{ visibility: "hidden" }}>some text here</p>
+									<a class="btn btn-sm" href="https://kerry.dev/draw/" target="blank">View Live</a>
+									<a class="btn btn-sm" href="https://github.com/kerryja/JS30/tree/master/7%20-%20HTML%20Canvas" target="blank">GitHub</a>
+									<div class="text">Drawing app built with HTML Canvas and JS</div>
 								</div>
 							</div>
 						</div>
@@ -65,19 +62,39 @@ class Projects extends Component {
 							<div class="card">
 								<video src="circles.mp4" width="507.66" loop={true} alt="video of circles project" />
 								<div class="card-img-overlay">
-									<a class="btn btn-md" href="https://kerry.dev/circles/" target="blank">View Live</a>
-									<a class="btn btn-md" href="https://github.com/kerryja/Circles" target="blank">GitHub</a>
-									<p class="text">some text here</p>
+									<a class="btn btn-sm" href="https://kerry.dev/circles/" target="blank">View Live</a>
+									<a class="btn btn-sm" href="https://github.com/kerryja/Circles" target="blank">GitHub</a>
+									<div class="text">Circles animation built with PaperJS and HowlerJS</div>
 								</div>
 							</div>
 						</div>
 						<div class="col-md-6 col-lg-4">
 							<div class="card">
-								<img src="jamout.jpg" width="507.66" alt="video of circles project" />
+								<video src="circles.mp4" width="507.66" loop={true} alt="video of circles project" />
 								<div class="card-img-overlay">
-									<a class="btn btn-md" href="https://kerry.dev/jamout/" target="blank">View Live</a>
-									<a class="btn btn-md" href="https://github.com/kerryja/bloc-jams-react" target="blank">GitHub</a>
-									<p class="text">some text here</p>
+									<a class="btn btn-sm" href="https://kerry.dev/circles/" target="blank">View Live</a>
+									<a class="btn btn-sm" href="https://github.com/kerryja/Circles" target="blank">GitHub</a>
+									<div class="text">some text here</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-lg-4">
+							<div class="card">
+								<video src="circles.mp4" width="507.66" loop={true} alt="video of circles project" />
+								<div class="card-img-overlay">
+									<a class="btn btn-sm" href="https://kerry.dev/circles/" target="blank">View Live</a>
+									<a class="btn btn-sm" href="https://github.com/kerryja/Circles" target="blank">GitHub</a>
+									<div class="text">some text here</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-lg-4">
+							<div class="card">
+								<video src="circles.mp4" width="507.66" loop={true} alt="video of circles project" />
+								<div class="card-img-overlay">
+									<a class="btn btn-sm" href="https://kerry.dev/circles/" target="blank">View Live</a>
+									<a class="btn btn-sm" href="https://github.com/kerryja/Circles" target="blank">GitHub</a>
+									<div class="text">some text here</div>
 								</div>
 							</div>
 						</div>
